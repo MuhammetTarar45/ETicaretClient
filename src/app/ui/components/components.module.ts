1a51966b2c468cc04ca997bf6eb8c586249a331e
Muhammet Tarar <126671170+MuhammetTarar45@users.noreply.github.com>
2025-03-19 16:40:22 +0300

    lesson12

---
+++ src/app/ui/components/components.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsModule } from './products/products.module';
+import { HomeModule } from './home/home.module';
+import { BasketsModule } from './baskets/baskets.module';
+import { ErrorComponent } from './error/error.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    ErrorComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeModule,
+    ProductsModule,
+    BasketsModule
+  ]
+})
+export class ComponentsModule { }
